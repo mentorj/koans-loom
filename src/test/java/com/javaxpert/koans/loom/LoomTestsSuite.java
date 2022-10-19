b5d608853cf9d42d2005c90471d18305c9732d28
deadbrain <jerome@javaxpert.com>
2022-10-19 11:08:04 +0200

    adds a couple of tests

--- src/test/java/com/javaxpert/koans/loom/LoomTestsSuite.java
+++ src/test/java/com/javaxpert/koans/loom/LoomTestsSuite.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.ThreadFactory;
 import java.util.regex.Pattern;
 
 /**
@@ -65,5 +66,42 @@ class LoomTestsSuite {
     	Thread t = Thread.ofVirtual().start(r);
     	assertThrows(UnsupportedOperationException.class,() -> {t.stop();},"UnsuportedException expected here!!" );
     }
+    
+    @Test
+    void whenNameMatters() {
+    	Runnable r = runnableProvider.justFort1SecondTask;
+    	ThreadFactory factory = Thread.ofVirtual().name("MyTask").factory();
+    	
+    	for (int i=0;i< 10000;i++) {
+
+    		Thread t = factory.newThread(r);
+    		t.start();
+    		assertTrue(t.getName().contains("MyTask"));
+    	}
+    }
+    
+    @Test
+    void changePriorityVirtualThreadsShouldHaveNoEffect() {
+    	Runnable r = runnableProvider.getJustFort1SecondTask();
+    	Thread t = Thread.startVirtualThread(r);
+    	int initial_priority = t.getPriority();
+    	t.setPriority(Thread.MAX_PRIORITY);
+    	int after_priority = t.getPriority();
+    	assertEquals(initial_priority, after_priority);
+    	assertEquals(initial_priority, Thread.NORM_PRIORITY);
+    }
+    
+    
+    @Test
+    void virtualThreadsDoNotSupportGroupNameOldFeature() {
+    	Runnable r = runnableProvider.getJustFort1SecondTask();
+    	Thread  t = Thread.ofVirtual().start(r);
+    	int threads_in_group = t.getThreadGroup().activeCount();
+    	System.err.println("Number of threads in group "+ threads_in_group);
+    	assertEquals(0,threads_in_group);
+    	
+    }
+    
+    
 
 }
