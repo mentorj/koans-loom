a54bffa08b2d23ca49bde634b0d8bd8f231c6c61
deadbrain <jerome@javaxpert.com>
2022-10-20 10:38:51 +0200

    makes all tests RED and adds doc

--- src/test/java/com/javaxpert/koans/loom/LoomTestsSuite.java
+++ src/test/java/com/javaxpert/koans/loom/LoomTestsSuite.java
@@ -44,7 +44,7 @@ class LoomTestsSuite {
         t.start();
         int afterStartActiveThreads =MultipleRunnables.getActiveThreadsCount();
         assertEquals(true,t.isAlive());
-        assertEquals(1,afterStartActiveThreads-initialActiveThreads);
+        assertEquals(0,afterStartActiveThreads-initialActiveThreads);
 
     }
 
@@ -58,7 +58,7 @@ class LoomTestsSuite {
         int afterStartActiveThreads =MultipleRunnables.getActiveThreadsCount();
         assertEquals(true,t.isAlive());
         assertEquals(true,t.isVirtual());
-        assertEquals(0,afterStartActiveThreads-initialActiveThreads);
+        assertEquals(1,afterStartActiveThreads-initialActiveThreads);
     }
 
     @Test
@@ -66,15 +66,14 @@ class LoomTestsSuite {
         Runnable r = runnableProvider.shortLifeTask;
         Thread t = Thread.ofVirtual().unstarted(r);
         String thread_desc= t.toString();
-        System.err.println("Thread desc ="+ thread_desc);
-        assertTrue(thread_desc.contains("VirtualThrea") );
+        assertTrue(thread_desc.contains("Blah") );
     }
 
     @Test
     void stoppingVirtualThreadsShouldRaiseException() {
     	Runnable r = runnableProvider.getJustFort1SecondTask();
     	Thread t = Thread.ofVirtual().start(r);
-    	assertThrows(UnsupportedOperationException.class,() -> {t.stop();},"UnsuportedException expected here!!" );
+    	assertThrows(NullPointerException.class,() -> {t.stop();},"Virtual Threads  can't be stopped.." );
     }
     
     @Test
@@ -86,7 +85,7 @@ class LoomTestsSuite {
 
     		Thread t = factory.newThread(r);
     		t.start();
-    		assertTrue(t.getName().contains("MyTask"));
+    		assertTrue(t.getName().contains("MyThread"));
     	}
     }
     
@@ -98,7 +97,7 @@ class LoomTestsSuite {
     	t.setPriority(Thread.MAX_PRIORITY);
     	int after_priority = t.getPriority();
     	assertEquals(initial_priority, after_priority);
-    	assertEquals(initial_priority, Thread.NORM_PRIORITY);
+    	assertEquals(after_priority, Thread.MAX_PRIORITY,"Can I change priority for Virtual Threads?");
     }
     
     
@@ -107,8 +106,8 @@ class LoomTestsSuite {
     	Runnable r = runnableProvider.getJustFort1SecondTask();
     	Thread  t = Thread.ofVirtual().start(r);
     	int threads_in_group = t.getThreadGroup().activeCount();
-    	System.err.println("Number of threads in group "+ threads_in_group);
-    	assertEquals(0,threads_in_group);
+    	
+    	assertEquals(1,threads_in_group,"Virtual threads & groups");
     	
     }
     
@@ -121,8 +120,8 @@ class LoomTestsSuite {
     		  	
     	}
     	long afterCreationThreadsCount = ManagementFactory.getThreadMXBean().getTotalStartedThreadCount();
-    	assertTrue(afterCreationThreadsCount>=initialThreadsCount,"Some new threads should have been created");
-    	assertEquals(initialThreadsCount,afterCreationThreadsCount,"what is the number of threads created?");
+    	assertTrue(afterCreationThreadsCount==0,"Some new threads should have been created?");
+    	assertEquals(0,afterCreationThreadsCount,"what is the number of threads created?");
     	
     }
     
@@ -138,7 +137,7 @@ class LoomTestsSuite {
     	.map(id -> threadMX.getThreadInfo(id))
     	.filter(info -> info.getThreadName().contains("Eternal"))
     	.count();
-    	assertEquals(0l, active_virtual_threads,"expect to see as many Virtual Threads as started");
+    	assertEquals(100l, active_virtual_threads,"expect to see as many Virtual Threads as started");
     }
     
     @Test
@@ -150,7 +149,7 @@ class LoomTestsSuite {
     
 		var stackTracesMap = Thread.getAllStackTraces();
 		long vThhreadsNumber = stackTracesMap.keySet().stream().filter(thread -> thread.getName().contains("Eternal"))	.count();
-		assertEquals(0l,vThhreadsNumber,"stacktrace should enable to find virtual threads");
+		assertEquals(100l,vThhreadsNumber,"stacktrace should enable to find virtual threads");
     }
     
     
@@ -165,7 +164,7 @@ class LoomTestsSuite {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-    	        counter.incrementAndGet();
+    	        
     	      });
     	      
     	    });
@@ -174,7 +173,7 @@ class LoomTestsSuite {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-    	    assertEquals(10000,counter.get(),"Counter should host as many tasks launched");
+    	    assertEquals(-1,counter.get(),"Counter should host as many tasks launched");
     	  }
     	    
     }
